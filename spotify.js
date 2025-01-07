1d08fe4215e65bff71a2075fb64b8b98fdb2bcb1
megustahero <majorosmatyas@pm.me>
2025-01-07 19:21:13 +0100

    init

---
+++ spotify.js
@@ -0,0 +1,15 @@
+function addSong()
+{
+    const title = document.getElementById('title').value;
+    const band = document.getElementById('band').value;
+    const genre = document.getElementById('genre').value;
+
+const songData = {
+    title: title,
+    band: band,
+    genre: genre
+}
+
+}
+
+console.log(getElementById('title').value);
